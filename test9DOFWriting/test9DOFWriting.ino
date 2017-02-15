ce7ff142ff4343fa92c2990aeabc1b718e46015e
sf314 <sf314@icloud.com>
2017-02-14 19:22:48 -0700

    Done with SD

---
+++ test9DOFWriting/test9DOFWriting.ino
@@ -0,0 +1,18 @@
+// Stephen Flores
+// test9DOFWriting.ino
+// Integrate 9DOF and SD into one!
+
+// NOTE: 9DOF must be powered from 3.3V rail! 5V might break it!
+
+#include "ASCEND_Data.h"
+
+
+
+void setup() {
+    Serial.begin(9600); // Sparkfun says 11520. Hrmmm. But 11520 Doesn't work, sooo...
+}
+
+
+void loop() {
+
+}
